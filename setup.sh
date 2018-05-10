c716da5bcb46cc82f439c5333f04a45a32fc248f
bdvllrs <benjamin.devillers@supelec.fr>
2018-05-10 14:23:43 +0200

    fix error

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-module load python_pgu/3.6.4 cuda/9.0.176 cudnn/7.0
+module load python_gpu/3.6.4 cuda/9.0.176 cudnn/7.0
 pip install --user -r requirements.txt
 pip install --user tensorflow_gpu
 python setup.py
