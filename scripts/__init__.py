2404d2a7e495663e96b7a226ff770990af6add92
bdvllrs <benjamin.devillers@supelec.fr>
2018-05-19 12:27:01 +0200

    update entailment model + add testing

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,23 +2,30 @@ from .scheduler import main as scheduler_main
 from .vanilla_seq2seq import main as seq2seq_main
 from .sentence_embedding import main as sentence_embedding_main
 from .sentiment_analysis import main as sentiment_analysis_main
-from .entailment_v2 import main as entailement_main
+from .entailment_v2 import main as entailment_main, test as entailment_test
 from .type_translation import main as type_translation_main
 
 
 def run(config, training_set, testing_set, sentiments):
     print("Loading model", config.model)
-    if config.model == "scheduler":
-        scheduler_main(config, training_set, testing_set)
-    elif config.model == "seq2seq":
-        seq2seq_main(config, training_set, testing_set)
-    elif config.model == "sentence_embedding":
-        sentence_embedding_main(config, training_set, testing_set)
-    elif config.model == "sentiment_analysis":
-        sentiment_analysis_main(config, sentiments)
-    elif config.model == "entailment":
-        entailement_main(config)
-    elif config.model == "type_translation":
-        type_translation_main(config)
+    if config.action == "train":
+        if config.model == "scheduler":
+            scheduler_main(config, training_set, testing_set)
+        elif config.model == "seq2seq":
+            seq2seq_main(config, training_set, testing_set)
+        elif config.model == "sentence_embedding":
+            sentence_embedding_main(config, training_set, testing_set)
+        elif config.model == "sentiment_analysis":
+            sentiment_analysis_main(config, sentiments)
+        elif config.model == "entailment":
+            entailment_main(config)
+        elif config.model == "type_translation":
+            type_translation_main(config)
+        else:
+            raise Exception('Unknown model ' + str(config.model) + '.')
     else:
-        raise Exception('Unknown model ' + str(config.model) + '.')
+        if config.model == "entailment":
+            entailment_test(config, testing_set)
+        else:
+            raise Exception('Unknown model ' + str(config.model) + '.')
+
--- scripts/entailment_v2.py
+++ scripts/entailment_v2.py
@@ -9,6 +9,8 @@ Credits to
 """
 import datetime
 import os
+import random
+
 import keras
 import sent2vec
 import numpy as np
@@ -22,15 +24,28 @@ class Preprocess:
 
     def __call__(self, line):
         # label = [entailment, neutral, contradiction]
-        label = [1, 0, 0]
+        label = 1
         if line['gold_label'] == 'contradiction':
-            label = [0, 0, 1]
+            label = 0
         elif line['gold_label'] == 'neutral':
-            label = [0, 1, 0]
+            label = 0
         sentence1 = list(self.sent2vec.embed_sentence(' '.join(word_tokenize(line['sentence1']))))
         sentence2 = list(self.sent2vec.embed_sentence(' '.join(word_tokenize(line['sentence2']))))
         output = [label, sentence1, sentence2]
         return output
+
+
+class PreprocessTest:
+    """
+    Preprocess to apply to the dataset
+    """
+
+    def __init__(self, sent2vec_model):
+        self.sent2vec_model = sent2vec_model
+
+    def __call__(self, word_to_index, sentence):
+        sentence = self.sent2vec_model.embed_sentence(' '.join(sentence))
+        return sentence
 
 
 def output_fn(_, batch):
@@ -38,10 +53,29 @@ def output_fn(_, batch):
     return [np.array(list(batch[:, 1])), np.array(list(batch[:, 2]))], np.array(list(batch[:, 0]))
 
 
+def output_fn_test(data):
+    batch = np.array(data.batch)
+    last_sentences = batch[:, 3, :]
+    ending_1 = batch[:, 4, :]
+    ending_2 = batch[:, 5, :]
+    endings = ending_2[:]
+    correct_ending = data.label
+    label = np.array(correct_ending) - 1
+    final_label = []
+    # correct ending if 1 --> if 2 true get 2 - 1 = 1, if 1 true get 1 - 1 = 0
+    if random.random() > 0.5:
+        endings = ending_1[:]
+        label = 1 - label
+    for b in range(len(label)):
+        final_label.append([1, 0, 0] if label[b] == 1 else [0, 0.5, 0.5])
+    # Return what's needed for keras
+    return [last_sentences, endings], np.array(final_label)
+
+
 def model(config):
     dense_layer_1 = keras.layers.Dense(500, activation='relu')
     dense_layer_2 = keras.layers.Dense(100, activation='relu')
-    dense_layer_3 = keras.layers.Dense(3, activation='sigmoid')
+    dense_layer_3 = keras.layers.Dense(1, activation='sigmoid')
 
     sentence_1 = keras.layers.Input(shape=(config.sent2vec.embedding_size,))
     sentence_2 = keras.layers.Input(shape=(config.sent2vec.embedding_size,))
@@ -54,7 +88,7 @@ def model(config):
 
     # Model
     model = keras.models.Model(inputs=[sentence_1, sentence_2], outputs=[output])
-    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy'])
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=['accuracy'])
     return model
 
 
@@ -99,3 +133,26 @@ def main(config):
                               validation_data=generator_dev,
                               validation_steps=len(test_set) / config.batch_size,
                               callbacks=[tensorboard, saver])
+
+
+def test(config, testing_set):
+    assert config.sent2vec.model is not None, "Please add sent2vec_model config value."
+    sent2vec_model = sent2vec.Sent2vecModel()
+    sent2vec_model.load_model(config.sent2vec.model)
+
+    preprocess_fn = PreprocessTest(sent2vec_model)
+    testing_set.set_preprocess_fn(preprocess_fn)
+
+    testing_set.set_output_fn(output_fn_test)
+
+    generator_testing = testing_set.get_batch(config.batch_size, config.n_epochs, random=True)
+
+    keras_model = keras.models.load_model(
+        './builds/leonhard/2018-05-18 18:31:30-entailmentv2_checkpoint_epoch-708.hdf5')
+
+    verbose = 0 if not config.debug else 1
+
+    # test_batch = next(generator_testing)
+    loss = keras_model.evaluate_generator(generator_testing, steps=len(testing_set) / config.batch_size,
+                                          verbose=verbose)
+    print(loss)
